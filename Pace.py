6054515c21a4eafc574d4dc4a0ebc6c8d92aaec9
Henglin Yang <henglinyang@yahoo.com>
2017-07-17 05:50:28 -0400

    Fix bugs due to python 2 to 3 change

--- Pace.py
+++ Pace.py
@@ -27,12 +27,12 @@ class Pace(object):
         if time < 3600:
             s = time % 60
             if s < 10:
-                return str(time/60) + ':0' + str(s)
-            return str(time/60) + ':' + str(s)
+                return str(time//60) + ':0' + str(s)
+            return str(time//60) + ':' + str(s)
         s = time % 60
         m = time % 3600
-        m /= 60
-        time = str(time / 3600)
+        m //= 60
+        time = str(time // 3600)
         if m < 10:
             time = time + ':0' + str(m)
         else:
@@ -74,52 +74,52 @@ class Pace(object):
 
     @property
     def mile(self):
-        return self.seconds_to_time(int(self._seconds * self._mile.value / self._meters))
+        return self.seconds_to_time(int(self._seconds * self._mile.value // self._meters))
 
     @property
     def full(self):
-        return self.seconds_to_time(int(self._seconds * self._marathon.value / self._meters))
+        return self.seconds_to_time(int(self._seconds * self._marathon.value // self._meters))
 
     @property
     def half(self):
-        return self.seconds_to_time(int(self._seconds * self._marathon.value / self._meters / 2))
+        return self.seconds_to_time(int(self._seconds * self._marathon.value // self._meters // 2))
 
     @property
     def tenK(self):
-        return self.seconds_to_time(int(self._seconds * 10000 / self._meters))
+        return self.seconds_to_time(int(self._seconds * 10000 // self._meters))
     tenk = tenK
 
     @property
     def fiveK(self):
-        return self.seconds_to_time(int(self._seconds * 5000 / self._meters))
+        return self.seconds_to_time(int(self._seconds * 5000 // self._meters))
     fivek = fiveK
 
     @property
     def K5(self):
-        return self.seconds_to_time(int(self._seconds * 1500 / self._meters))
+        return self.seconds_to_time(int(self._seconds * 1500 // self._meters))
     k5 = K5
 
     @property
     def Km(self):
-        return self.seconds_to_time(int(self._seconds * 1000 / self._meters))
+        return self.seconds_to_time(int(self._seconds * 1000 // self._meters))
     km = Km
 
     def pace(self, distance):
         distance = str(distance)
-        return self.seconds_to_time(int(self._seconds * self.distance_to_meter(distance) / self._meters))
+        return self.seconds_to_time(int(self._seconds * self.distance_to_meter(distance) // self._meters))
 
 if __name__ == '__main__':
     argv = sys.argv[1:]
     argc = len(argv)
     if argc == 0:
-        print 'Usage: python pace.py <full marathon time>'
-        print '       python pace.py <distance> <time>'
+        print('Usage: python pace.py <full marathon time>')
+        print('       python pace.py <distance> <time>')
         quit()
     if argc == 1:
         p = Pace('full', argv[0])
     else:
         p = Pace(argv[0], argv[1])
-    print 'Pace ' + p.mile + ', Full ' + p.full + ', 10k ' + p.tenK + ', 5k ' + p.fiveK + ', 1500m ' + p.K5 + ', 1000m ' + p.Km
+    print('Pace ' + p.mile + ', Full ' + p.full + ', 10k ' + p.tenK + ', 5k ' + p.fiveK + ', 1500m ' + p.K5 + ', 1000m ' + p.Km)
         
 
 
--- TrainWeeks.py
+++ TrainWeeks.py
@@ -9,30 +9,32 @@ class TrainWeeks(object):
 
     @property
     def race_day(self):
-        return '{m}/{d}/{y}'.format(m=self._race.month, d=self._race.day, y=self._race.year)
+        return '{m}/{d}/{y}'.format(
+                m=self._race.month, d=self._race.day, y=self._race.year)
     
     @property
     def training_start_day(self):
-        return '{m}/{d}/{y}'.format(m=self._start.month, d=self._start.day, y=self._start.year)
+        return '{m}/{d}/{y}'.format(
+                m=self._start.month, d=self._start.day, y=self._start.year)
 
     @property
     def duration(self):
         return self._duration.days
 
     def __str__(self):
-        return 'Training of {d} days starts at {s} for race day {r} '.format(r=self.race_day,
-                                                                        s=self.training_start_day,
-                                                                        d=self.duration)
+        return 'Training of {d} days starts at {s} for race day {r} '.format(
+                r=self.race_day, s=self.training_start_day, d=self.duration)
 
     def __repr__(self):
-        return 'TrainWeeks({m}, {d}, {y}, nr_days={u})'.format(m=self._race.month,
-                                                               d=self._race.day,
-                                                               y=self._race.year,
-                                                               u=self._duration.days)
+        return 'TrainWeeks({m}, {d}, {y}, nr_days={u})'.format(
+                m=self._race.month,
+                d=self._race.day,
+                y=self._race.year,
+                u=self._duration.days)
 if __name__ == '__main__':
     def usage_and_exit():
-        print 'Today is {t}.\nUsage: {c} <mm/dd/yy> <number of weeks>'.format(t=date.today(),
-                                                                            c=argv[0].split('/')[-1])
+        print('Today is {t}.\nUsage: {c} <mm/dd/yy> <number of weeks>'.format(
+            t=date.today(), c=argv[0].split('/')[-1]))
         exit()
     argc = len(argv)
     if argc == 1:
@@ -42,9 +44,13 @@ if __name__ == '__main__':
         usage_and_exit()
     if len(date_minutes) < 3:
         date_minutes.append(str(date.today().year))
-    print date_minutes
     if argc < 3:
-        tw = TrainWeeks(int(date_minutes[0]), int(date_minutes[1]), int(date_minutes[2]))
+        tw = TrainWeeks(int(date_minutes[0]),
+                        int(date_minutes[1]),
+                        int(date_minutes[2]))
     else:
-        tw = TrainWeeks(int(date_minutes[0]), int(date_minutes[1]), int(date_minutes[2]), nr_weeks=int(argv[2]))
-    print tw
+        tw = TrainWeeks(int(date_minutes[0]),
+                        int(date_minutes[1]),
+                        int(date_minutes[2]),
+                        nr_weeks=int(argv[2]))
+    print(tw)
